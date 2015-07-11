e87b19cf4738a43f32c0fd3c99ab2f9ae462e67e
git <brother79@mail.ru>
2015-07-11 14:48:56 +0600

    Fix

---
+++ Listener/ResponseListener.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brother\CMSBundle\Listener;
+
+use Sonata\PageBundle\Listener\ResponseListener as BaseResponseListener;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+/**
+ * This class redirect the onCoreResponse event to the correct
+ * cms manager upon user permission.
+ *
+ * @author Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ */
+class ResponseListener extends BaseResponseListener
+{
+
+    public function onCoreResponse(FilterResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        if (!$response->isRedirection()) {
+            parent::onCoreResponse($event);
+        }
+    }
+}
